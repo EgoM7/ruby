44b637858c4ff39536aea45359600534e4c88db3
EgoM7 <goguillot.maxime@gmail.com>
2023-10-03 19:33:04 +0200

    ruby

--- exoruby1/exo_03.rb
+++ exoruby1/exo_03.rb
@@ -1,7 +1,6 @@
 puts "Quelle est votre année de naissance?"
 birth_year = gets.chomp
 def age
-  now = Time.now.utc.to_date
-  now.year - birthday.year - (birthday.to_date.change(:year => now.year) > now ? 1 : 0)
+  
 end
-puts "en 2017 tu avais" 
+puts "en 2017" + age + "tu avais" 
